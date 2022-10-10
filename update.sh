6161c2b6dc1abf8a44c0e9191aea9f5190ad98f3
gnikit <giannis.nikiteas@gmail.com>
2022-10-10 14:23:31 +0100

    feat: add programs
    FYI fpm has a bug see https://github.com/fortran-lang/fpm/pull/775
    where C preprocessor flags do not propagate into
    the executable. The way to circumvent this is by
    passing both --flag and --c-flag options during run/build/etc.

--- update.sh
+++ update.sh
@@ -14,6 +14,41 @@ mkdir -p $DEST/include
 cp -f $SRC/LICENSE $DEST/LICENSE
 cp -fa $SRC/include/*.h $DEST/include/
 cp -fa $SRC/libmetis/*.{h,c} $DEST/libmetis/
+cp -fa $SRC/graphs ./
+
+mkdir -p "app"
+mkdir -p "app/cmpfillin"
+mkdir -p "app/gpmetis"
+mkdir -p "app/graphchk"
+mkdir -p "app/m2gmetis"
+mkdir -p "app/mpmetis"
+mkdir -p "app/ndmetis"
+mkdir -p "app/utils"
+
+cp -fa $SRC/programs/*.{h,c} "app/utils"
+mv app/utils/cmpfillin.c app/cmpfillin
+
+mv app/utils/gpmetis.c app/gpmetis
+mv app/utils/cmdline_gpmetis.c app/gpmetis
+
+mv app/utils/graphchk.c app/graphchk
+
+mv app/utils/m2gmetis.c app/m2gmetis
+mv app/utils/cmdline_m2gmetis.c app/m2gmetis
+
+mv app/utils/mpmetis.c app/mpmetis
+mv app/utils/cmdline_mpmetis.c app/mpmetis
+
+mv app/utils/ndmetis.c app/ndmetis
+mv app/utils/cmdline_ndmetis.c app/ndmetis
+
+# Hard link the remaining files in utils
+ln -f app/utils/* app/cmpfillin
+ln -f app/utils/* app/gpmetis
+ln -f app/utils/* app/graphchk
+ln -f app/utils/* app/m2gmetis
+ln -f app/utils/* app/mpmetis
+ln -f app/utils/* app/ndmetis
 
 # Remove files that are not needed
 rm -rf $SRC
